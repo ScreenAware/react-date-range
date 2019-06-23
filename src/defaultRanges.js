ae6df3bbec56612a3843a71c3c87caed38a3838b
Hinnerk Bruegmann <h.bruegmann@gmail.com>
2019-06-23 21:59:11 +0200

    use function for defined datetimes so values for today, yesterday etc are recomputed on the fly

--- src/defaultRanges.js
+++ src/defaultRanges.js
@@ -12,18 +12,18 @@ import {
 } from 'date-fns';
 
 const defineds = {
-  startOfWeek: startOfWeek(new Date()),
-  endOfWeek: endOfWeek(new Date()),
-  startOfLastWeek: startOfWeek(addDays(new Date(), -7)),
-  endOfLastWeek: endOfWeek(addDays(new Date(), -7)),
-  startOfToday: startOfDay(new Date()),
-  endOfToday: endOfDay(new Date()),
-  startOfYesterday: startOfDay(addDays(new Date(), -1)),
-  endOfYesterday: endOfDay(addDays(new Date(), -1)),
-  startOfMonth: startOfMonth(new Date()),
-  endOfMonth: endOfMonth(new Date()),
-  startOfLastMonth: startOfMonth(addMonths(new Date(), -1)),
-  endOfLastMonth: endOfMonth(addMonths(new Date(), -1)),
+  startOfWeek: () => startOfWeek(new Date()),
+  endOfWeek: () => endOfWeek(new Date()),
+  startOfLastWeek: () => startOfWeek(addDays(new Date(), -7)),
+  endOfLastWeek: () => endOfWeek(addDays(new Date(), -7)),
+  startOfToday: () => startOfDay(new Date()),
+  endOfToday: () => endOfDay(new Date()),
+  startOfYesterday: () => startOfDay(addDays(new Date(), -1)),
+  endOfYesterday: () => endOfDay(addDays(new Date(), -1)),
+  startOfMonth: () => startOfMonth(new Date()),
+  endOfMonth: () => endOfMonth(new Date()),
+  startOfLastMonth: () => startOfMonth(addMonths(new Date(), -1)),
+  endOfLastMonth: () => endOfMonth(addMonths(new Date(), -1)),
 };
 
 const staticRangeHandler = {
@@ -45,44 +45,44 @@ export const defaultStaticRanges = createStaticRanges([
   {
     label: 'Today',
     range: () => ({
-      startDate: defineds.startOfToday,
-      endDate: defineds.endOfToday,
+      startDate: defineds.startOfToday(),
+      endDate: defineds.endOfToday(),
     }),
   },
   {
     label: 'Yesterday',
     range: () => ({
-      startDate: defineds.startOfYesterday,
-      endDate: defineds.endOfYesterday,
+      startDate: defineds.startOfYesterday(),
+      endDate: defineds.endOfYesterday(),
     }),
   },
 
   {
     label: 'This Week',
     range: () => ({
-      startDate: defineds.startOfWeek,
-      endDate: defineds.endOfWeek,
+      startDate: defineds.startOfWeek(),
+      endDate: defineds.endOfWeek(),
     }),
   },
   {
     label: 'Last Week',
     range: () => ({
-      startDate: defineds.startOfLastWeek,
-      endDate: defineds.endOfLastWeek,
+      startDate: defineds.startOfLastWeek(),
+      endDate: defineds.endOfLastWeek(),
     }),
   },
   {
     label: 'This Month',
     range: () => ({
-      startDate: defineds.startOfMonth,
-      endDate: defineds.endOfMonth,
+      startDate: defineds.startOfMonth(),
+      endDate: defineds.endOfMonth(),
     }),
   },
   {
     label: 'Last Month',
     range: () => ({
-      startDate: defineds.startOfLastMonth,
-      endDate: defineds.endOfLastMonth,
+      startDate: defineds.startOfLastMonth(),
+      endDate: defineds.endOfLastMonth(),
     }),
   },
 ]);
